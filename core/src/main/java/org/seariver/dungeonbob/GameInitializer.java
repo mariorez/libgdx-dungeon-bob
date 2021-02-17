39f21b9b0b017bd49e2e2ac8ad5edf592c4164e6
Mario Rezende <mariorez@gmail.com>
2021-02-17 09:37:35 -0300

    first commit

---
+++ core/src/main/java/org/seariver/dungeonbob/GameInitializer.java
@@ -0,0 +1,6 @@
+package org.seariver.dungeonbob;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class GameInitializer extends ApplicationAdapter {
+}
---
+++ lwjgl3/src/main/java/org/seariver/dungeonbob/lwjgl3/DesktopLauncher.java
@@ -0,0 +1,26 @@
+package org.seariver.dungeonbob.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import org.seariver.dungeonbob.GameInitializer;
+
+/**
+ * Launches the desktop (LWJGL3) application.
+ */
+public class DesktopLauncher {
+    public static void main(String[] args) {
+        createApplication();
+    }
+
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new GameInitializer(), getDefaultConfiguration());
+    }
+
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setTitle("DungeonBob");
+        configuration.setWindowedMode(800, 600);
+        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        return configuration;
+    }
+}
