ae99ad6db54643dadc8021af92f78f7641adc2e5
Mario Rezende <mariorez@gmail.com>
2021-02-17 18:55:26 -0300

    implements bob movements

--- core/src/main/java/org/seariver/dungeonbob/gameobjects/Bob.java
+++ core/src/main/java/org/seariver/dungeonbob/gameobjects/Bob.java
@@ -2,27 +2,39 @@ package org.seariver.dungeonbob.gameobjects;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
 
 public class Bob {
 
     public Sprite bobSprite; //sprite to display Bob
     private static final float X_MOVE_UNITS = 3f;// units bob will move in x direction
 
+    Animation<TextureRegion> walkAnimation;
+    Texture walkSheet;
+    TextureRegion currentFrame;
+    float stateTime;
+
+    public static final float BOB_RESIZE_FACTOR = 750f;
+    private static int ANIMATION_FRAME_SIZE = 8; // this specifies the number of frames(images) that we are using for animation
+    private static float ANIMATION_TIME_PERIOD = 0.08f;// this specifies the time between two consecutive frames of animation
+
     public void update() {
-        // move specified units to left if left key is pressed
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             move(-X_MOVE_UNITS, 0);
-        }
-
-        // move specified units to right if right key is pressed
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             move(X_MOVE_UNITS, 0);
+        } else {
+            currentFrame = walkAnimation.getKeyFrame(0, true);
         }
     }
 
     public void render(SpriteBatch batch) {
+        bobSprite.setRegion(currentFrame);
         bobSprite.draw(batch);
     }
 
@@ -32,5 +44,31 @@ public class Bob {
 
     public void move(float x, float y) {
         setPosition(bobSprite.getX() + x, bobSprite.getY() + y);
+        stateTime += Gdx.graphics.getDeltaTime();
+        currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+    }
+
+    public void initialize(float width, float height, Texture walkSheet) {
+        this.walkSheet = walkSheet; // save the sprite sheet
+
+        // instantiate bob sprite
+        bobSprite = new Sprite();
+        bobSprite.setSize(
+                (walkSheet.getWidth() / ANIMATION_FRAME_SIZE) * (width / BOB_RESIZE_FACTOR),
+                walkSheet.getHeight() * (height / BOB_RESIZE_FACTOR));
+        setPosition(width / 2f, 0);
+
+        //split the sprite sheet into different textures
+        TextureRegion[][] tmp = TextureRegion.split(walkSheet, walkSheet.getWidth() / ANIMATION_FRAME_SIZE, walkSheet.getHeight());
+        Array<TextureRegion> textureArray = new Array<>();
+        for (int c = 0; c < ANIMATION_FRAME_SIZE; c++) {
+            textureArray.add(tmp[0][c]);
+        }
+        // create a new animation sequence with the walk frames and time period of specified seconds
+        walkAnimation = new Animation(ANIMATION_TIME_PERIOD, textureArray, Animation.PlayMode.LOOP);
+        // set the animation to loop
+        walkAnimation.setPlayMode(Animation.PlayMode.LOOP);
+        // get initial frame
+        currentFrame = walkAnimation.getKeyFrame(stateTime, true);
     }
 }
--- core/src/main/java/org/seariver/dungeonbob/managers/GameManager.java
+++ core/src/main/java/org/seariver/dungeonbob/managers/GameManager.java
@@ -9,26 +9,19 @@ import org.seariver.dungeonbob.gameobjects.Bob;
 public class GameManager {
 
     static Bob bob; // bob instance
-    static Texture bobTexture; // texture image for the bob
+    static Texture bobSpriteSheet; // texture sprite sheet for the bob
     public static Sprite backgroundSprite; // background sprite
     public static Texture backgroundTexture; // texture image for the background
-    public static final float BOB_RESIZE_FACTOR = 800f;
 
     public static void initialize(float width, float height) {
         // instantiate the bob
         bob = new Bob();
-        // load the bob texture with image from file
-        bobTexture = new Texture(Gdx.files.internal("bob.png"));
-        // instantiate bob sprite
-        bob.bobSprite = new Sprite(bobTexture);
-        //set the size of the bob
-        bob.bobSprite.setSize(bob.bobSprite.getWidth() * (width / BOB_RESIZE_FACTOR), bob.bobSprite.getHeight() * (width / BOB_RESIZE_FACTOR)); // set the position of bob to bottom - center
-        bob.setPosition(width / 2f, 0);
+        bobSpriteSheet = new Texture(Gdx.files.internal("bob_spritesheet.png"));
+        bob.initialize(width, height, bobSpriteSheet);
+
         //load background texture
         backgroundTexture = new Texture(Gdx.files.internal("background.png"));
-        //set background sprite with the texture
         backgroundSprite = new Sprite(backgroundTexture);
-        // set the background to completely fill the screen
         backgroundSprite.setSize(width, height);
     }
 
@@ -36,8 +29,8 @@ public class GameManager {
         // draw the background
         backgroundSprite.draw(batch);
 
+        // update and render (draw) the bob
         bob.update();
-        // Render(draw) the bob
         bob.render(batch);
     }
 
@@ -45,6 +38,6 @@ public class GameManager {
         //dispose the background texture
         backgroundTexture.dispose();
         // dispose of the bob texture to ensure no memory leaks
-        bobTexture.dispose();
+        bobSpriteSheet.dispose();
     }
 }
